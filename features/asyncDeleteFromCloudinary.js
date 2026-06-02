6fe3a64e560d282c17d00b0f1d20193ee9454c70
Tarun <tarunmalviya8827@gmail.com>
2026-06-02 17:48:24 +0530

    updated async delele from cloudinary

--- features/asyncDeleteFromCloudinary.js
+++ features/asyncDeleteFromCloudinary.js
@@ -1,7 +1,7 @@
 import deleteFromCloudinaryService from "../service/deleteFromCloudinary.service.js";
 
 const asyncDeleteFromCloudinary = (arr) => {
-    const res = Promise.all(arr?.map(async(ele) => await deleteFromCloudinaryService(ele.id, ele.type)));
+    const res = Promise.all(arr?.map(async (ele) => await deleteFromCloudinaryService(ele.id, ele.type)));
     return res;
 }
 export default asyncDeleteFromCloudinary;
