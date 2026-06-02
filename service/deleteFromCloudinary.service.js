52febaa1c248a91e5ee4de74358b81409c268605
Tarun <tarunmalviya8827@gmail.com>
2026-06-02 15:13:35 +0530

    remove logs

--- service/deleteFromCloudinary.service.js
+++ service/deleteFromCloudinary.service.js
@@ -1,15 +1,11 @@
 import cloudinary from "../config/cloudinary.js";
 
 const deleteFromCloudinaryService = async(id, type) => {
-    try {
         const res = await cloudinary.uploader.destroy(id,
             {
                 resource_type: type,
             });
         return res;
-    } catch (error) {
-        console.log("error in deleting from cloudinary", error);
-    }
 }
 
 export default deleteFromCloudinaryService;
--- service/uploadToCloudinary.service.js
+++ service/uploadToCloudinary.service.js
@@ -1,7 +1,6 @@
 import cloudinary from "../config/cloudinary.js";
 
 const uploadToCloudinaryService = async (path, type) => {
-    try {
         const result = await cloudinary.uploader.upload(path,
             {
                 folder: "real-state-rest-apis",
@@ -11,10 +10,7 @@ const uploadToCloudinaryService = async (path, type) => {
             url: result.url,
             id: result.public_id,
             type: result.resource_type,
-        }
-    } catch (error) {
-        console.log("error in deleting from cloudinary", error);
-    }
+        } 
 }
 
 export default uploadToCloudinaryService;
