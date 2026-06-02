dd21624fd6d78e9f575c2276b30da30c58f2ded1
Tarun <tarunmalviya8827@gmail.com>
2026-06-02 15:15:05 +0530

    remove logs

--- features/asyncUploadToCloudinary.js
+++ features/asyncUploadToCloudinary.js
@@ -1,7 +1,6 @@
 import uploadToCloudinaryService from "../service/uploadToCloudinary.service.js";
 
 const asyncUploadToCloudinary = (arr, type) => {
-    console.log(arr);
     const res = Promise.all(arr?.map(async(ele) => await uploadToCloudinaryService(ele.path, type)))
     return res;
 }
